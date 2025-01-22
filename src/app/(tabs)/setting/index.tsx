760b48593165adde7168060c2708db96af81ffe5
Doanhaiduy <haiduytbt2k3@gmail.com>
2025-01-22 15:05:38 +0700

    fix: revert eject

--- src/app/(tabs)/setting/index.tsx
+++ src/app/(tabs)/setting/index.tsx
@@ -1,13 +1,13 @@
-import authAPI from "@/apis/authApi";
-import { ContainerComponent, SectionComponent, TextComponent } from "@/components";
-import ImageComponent from "@/components/ImageComponent";
-import { colors } from "@/constants/colors";
-import { authSelector, logout } from "@/stores/reducers/authReducer";
-import { Entypo, Feather, FontAwesome, Ionicons } from "@expo/vector-icons";
-import { router } from "expo-router";
-import React from "react";
-import { Alert, Image, StyleSheet, Touchable, TouchableOpacity, View } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
+import authAPI from '@/apis/authApi';
+import { ContainerComponent, SectionComponent, TextComponent } from '@/components';
+import ImageComponent from '@/components/ImageComponent';
+import { colors } from '@/constants/colors';
+import { authSelector, logout } from '@/stores/reducers/authReducer';
+import { Entypo, Feather, FontAwesome, Ionicons } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import React from 'react';
+import { Alert, Image, StyleSheet, Touchable, TouchableOpacity, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function SettingPage() {
     const { authData } = useSelector(authSelector);
@@ -17,8 +17,10 @@ export default function SettingPage() {
     const handleLogout = async () => {
         try {
             const res = await authAPI.logout();
-            dispatch(logout());
-            router.navigate("/sign-in");
+            if (res) {
+                dispatch(logout());
+                router.navigate('/sign-in');
+            }
         } catch (error) {
             console.log(error);
         }
@@ -35,10 +37,10 @@ export default function SettingPage() {
                     <ImageComponent showImageModal url={authData?.avatar!} height={80} width={80} rounded={999} />
                 </View>
                 <View className="ml-4 flex-1">
-                    <TextComponent text={authData?.fullName || ""} className="text-xl" />
+                    <TextComponent text={authData?.fullName || ''} className="text-xl" />
                     <TextComponent text={`MSSV: ${authData?.username}`} className="text-base text-gray-400" />
                 </View>
-                <TouchableOpacity onPress={() => router.push("setting/profile")}>
+                <TouchableOpacity onPress={() => router.push('setting/profile')}>
                     <Feather name="info" size={24} color={colors.primary500} />
                 </TouchableOpacity>
             </SectionComponent>
@@ -46,7 +48,7 @@ export default function SettingPage() {
             <SectionComponent className="mt-4">
                 <TouchableOpacity
                     className="flex-row flex-1 w-full items-center py-6 border-y-[1px] border-y-text-200"
-                    onPress={() => router.push("setting/language")}
+                    onPress={() => router.push('setting/language')}
                 >
                     <Ionicons name="earth-outline" size={26} color={colors.primary400} />
                     <TextComponent text="Ngôn ngữ" className="text-base ml-4 flex-1" />
@@ -54,7 +56,7 @@ export default function SettingPage() {
                 </TouchableOpacity>
                 <TouchableOpacity
                     className="flex-row flex-1 w-full items-center py-6 border-b-[1px] border-b-text-200"
-                    onPress={() => router.push("setting/change-password")}
+                    onPress={() => router.push('setting/change-password')}
                 >
                     <Entypo name="lock" size={26} color={colors.primary400} />
                     <TextComponent text="Đổi mật khẩu" className="text-base ml-4 flex-1" />
@@ -62,7 +64,7 @@ export default function SettingPage() {
                 </TouchableOpacity>
                 <TouchableOpacity
                     className="flex-row flex-1 w-full items-center py-6 border-b-[1px] border-b-text-200"
-                    onPress={() => router.push("setting/helps")}
+                    onPress={() => router.push('setting/helps')}
                 >
                     <Feather name="info" size={26} color={colors.primary400} />
                     <TextComponent text="Trợ giúp" className="text-base ml-4 flex-1" />
@@ -71,13 +73,13 @@ export default function SettingPage() {
                 <TouchableOpacity
                     className="flex-row flex-1 w-full items-center py-6 border-b-[1px] border-b-text-200"
                     onPress={() => {
-                        Alert.alert("Thông báo", "Bạn có chắc chắn muốn đăng xuất?", [
+                        Alert.alert('Thông báo', 'Bạn có chắc chắn muốn đăng xuất?', [
                             {
-                                text: "Hủy",
-                                style: "cancel",
+                                text: 'Hủy',
+                                style: 'cancel',
                             },
                             {
-                                text: "Đồng ý",
+                                text: 'Đồng ý',
                                 onPress: () => handleLogout(),
                             },
                         ]);
--- src/constants/appInfo.ts
+++ src/constants/appInfo.ts
@@ -1,18 +1,18 @@
-import { Dimensions, Platform } from "react-native";
+import { Dimensions, Platform } from 'react-native';
 
 export const appInfo = Object.freeze({
     sizes: {
-        WIDTH: Dimensions.get("window").width,
-        HEIGHT: Dimensions.get("window").height,
+        WIDTH: Dimensions.get('window').width,
+        HEIGHT: Dimensions.get('window').height,
     },
     headerHomeBar: 0,
     headerHeight: {
-        default: Platform.OS === "android" ? 80 : 102,
-        onScroll: Platform.OS === "android" ? 80 : 102,
+        default: Platform.OS === 'android' ? 80 : 102,
+        onScroll: Platform.OS === 'android' ? 80 : 102,
     },
-    // base_url: 'http://192.168.154.193:3000/api/v1',
-    // base_view_url: 'http://192.168.154.193:3000/views',
+    // base_url: 'http://192.168.1.130:3000/api/v1',
+    // base_view_url: 'http://192.168.1.130:3000/views',
 
-    base_url: "https://nckhserver.duydemo.io.vn/api/v1",
-    base_view_url: "https://nckhserver.duydemo.io.vn/views",
+    base_url: 'https://nckhserver.duydemo.io.vn/api/v1',
+    base_view_url: 'https://nckhserver.duydemo.io.vn/views',
 });
